6b7329cc9f13429f0eb0afa6d30601db242bcdee
Your Name <you@example.com>
2023-05-23 16:18:16 -0600

    class name div

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,26 +6,26 @@ const Header: React.FC = () => {
     <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
       <div className="flex space-x-3">
         <Link href="/" passHref>
-          {/* <a className="flex items-center"> */}
           <div className="flex items-center">
-            <Image
-              alt="Custom Logo"
-              src="https://i.im.ge/2023/04/11/IBXuyY.EMDILOGO.png"
-              className="object-contain"
-              width={120}
-              height={48}
-            />
+            <div className="object-contain">
+              <Image
+                alt="Custom Logo"
+                src="https://i.im.ge/2023/04/11/IBXuyY.EMDILOGO.png"
+                width={120}
+                height={48}
+              />
+            </div>
           </div>
-          {/* </a> */}
         </Link>
       </div>
-      <Image
-        alt="Vercel Icon"
-        src="/vercelLogo.png"
-        className="sm:w-8 sm:h-[27px] w-8 h-[28px]"
-        width={32}
-        height={28}
-      />
+      <div className="sm:w-8 sm:h-[27px] w-8 h-[28px]">
+        <Image
+          alt="Vercel Icon"
+          src="/vercelLogo.png"
+          width={32}
+          height={28}
+        />
+      </div>
     </header>
   );
 };
